b01cdee5b2d03430ed2092a214dc5da0c79f7a09
Borhawn <borhawnm@gmail.com>
2025-05-27 20:37:10 +0330

    Last Edits

--- src/js/index.js
+++ src/js/index.js
@@ -1,56 +1,59 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const htmlElement = document.documentElement;
+document.addEventListener("DOMContentLoaded", () => {
+  const htmlElement = document.documentElement;
 
-    // Load saved theme preference or set default to dark
-    const savedTheme = localStorage.getItem('theme');
-    if (savedTheme === 'dark' || !savedTheme) {
-      htmlElement.classList.add('dark');
-      localStorage.setItem('theme', 'dark');
+  // Load saved theme preference or set default to dark
+  const savedTheme = localStorage.getItem("theme");
+  if (savedTheme === "dark" || !savedTheme) {
+    htmlElement.classList.add("dark");
+    localStorage.setItem("theme", "dark");
+  } else {
+    htmlElement.classList.remove("dark");
+  }
+
+  const toggleSwitch = document.querySelector(".toggle-switch");
+  const lightOption = document.getElementById("light");
+  const darkOption = document.getElementById("dark");
+  const slider = document.querySelector(".toggle-slider");
+
+  // Set toggleSwitch UI based on saved theme
+  if (htmlElement.classList.contains("dark")) {
+    darkOption.classList.add("active");
+    lightOption.classList.remove("active");
+    slider.style.transform = "translateX(0)";
+  } else {
+    lightOption.classList.add("active");
+    darkOption.classList.remove("active");
+    slider.style.transform = "translateX(100%)";
+  }
+
+  toggleSwitch.addEventListener("click", () => {
+    if (darkOption.classList.contains("active")) {
+      darkOption.classList.remove("active");
+      lightOption.classList.add("active");
+      slider.style.transform = "translateX(100%)";
+      htmlElement.classList.remove("dark");
+      localStorage.setItem("theme", "light");
     } else {
-      htmlElement.classList.remove('dark');
+      lightOption.classList.remove("active");
+      darkOption.classList.add("active");
+      slider.style.transform = "translateX(0)";
+      htmlElement.classList.add("dark");
+      localStorage.setItem("theme", "dark");
     }
-
-    const toggleSwitch = document.querySelector('.toggle-switch');
-    const lightOption = document.getElementById('light');
-    const darkOption = document.getElementById('dark');
-    const slider = document.querySelector('.toggle-slider');
-
-    // Set toggleSwitch UI based on saved theme
-    if (htmlElement.classList.contains('dark')) {
-        darkOption.classList.add('active');
-        lightOption.classList.remove('active');
-        slider.style.transform = 'translateX(0)';
-    } else {
-        lightOption.classList.add('active');
-        darkOption.classList.remove('active');
-        slider.style.transform = 'translateX(100%)';
-    }
-
-    toggleSwitch.addEventListener('click', () => {
-        if (darkOption.classList.contains('active')) {
-            darkOption.classList.remove('active');
-            lightOption.classList.add('active');
-            slider.style.transform = 'translateX(100%)';
-            htmlElement.classList.remove('dark');
-            localStorage.setItem('theme', 'light');
-        } else {
-            lightOption.classList.remove('active');
-            darkOption.classList.add('active');
-            slider.style.transform = 'translateX(0)';
-            htmlElement.classList.add('dark');
-            localStorage.setItem('theme', 'dark');
-        }
-    });
-    
+  });
 });
 
-    var swiper = new Swiper(".mySwiper", {
-      slidesPerView: 3,
-      spaceBetween: 30,
-      freeMode: true,
-            loop: true,
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-      },
-    });
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  freeMode: true,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
